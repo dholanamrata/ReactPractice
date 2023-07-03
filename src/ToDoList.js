524839cb3b7366701494ff95a0c8d63fc21b2dfc
dholanamrata <128412920+dholanamrata@users.noreply.github.com>
2023-07-03 13:05:45 +0530

    random

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,37 +1,72 @@
+import './ToDoList.css'
 import { useState } from "react";
-const listItem = ["Namrata", "Nayan", "Dhruti"];
+import { useImmer } from "use-immer";
+// const listItem = ["Namrata", "Nayan", "Dhruti"];
+const initialarry = [
+  { name: "namrata", id: 0 },
+  { name: "kajal", id: 1 },
+  { name: "pinal", id: 2 },
+  { name: "sejal", id: 3 },
+  { name: "gopi", id: 4 },
+];
+// const List = ({ data }) => {
+//   const list = data.map((data, index) => {
+//     return <li key={index}>{data}</li>;
+//   });
+//   return <ul>{list}</ul>;
+// };
 
-const List = ({ data }) => {
-  const list = data.map((data, index) => {
-    return <li key={index}>{data}</li>;
-  });
-  return <ul>{list}</ul>;
+const Items = ({ data, settingarry }) => {
+  const list = data.map((item) => (
+    <div className='list'>
+      <li key={item.id} >
+        <span className='spantag'>{item.name}</span>
+        {/* <i class="fa-solid fa-trash"></i> */}
+        <input
+          type="button"
+          name="delete"
+          value="Delete"
+          onClick={() => {
+            settingarry(
+              data.filter((a) => {
+                console.log(a.id);
+                return a.id !== item.id;
+              })
+            );
+          }}
+        />
+      </li>
+      <br />
+    </div>
+  ));
+  return <ol className='litage'>{list}</ol>;
 };
 
 const TodoList = () => {
-  const [value, setvalue] = useState("");
-  const [arry, setarry] = useState(listItem);
-
-  const handlechange = (e) => {
-    setvalue(e.target.value);
-  };
+  const [newid, setid] = useState(6);
+  const [value, setvalue] = useState({ name: "", id: 6 });
+  const [arry, setarry] = useState(initialarry);
 
   const handleclick = () => {
     setarry([...arry, value]);
-    setvalue("");
+    setvalue({ ...value, name: "" });
   };
+  const handlechange = (e) => {
+    setid(newid + 1);
+    setvalue({ ...value, name: e.target.value, id: newid });
+  };
+
   return (
-    <>
+    <div className='master'>
       <h1>To do list</h1>
       <input
         type="text"
-        value={value}
         onChange={handlechange}
         placeholder="add items..."
       />
-      <button onClick={handleclick}>Add</button>
-      <List data={arry} />
-    </>
+      <button class="custom-btn btn-7" onClick={handleclick}>Add</button>
+      <Items data={arry} settingarry={setarry} />
+    </div>
   );
 };
 export default TodoList;
--- src/UpdateObjWithSpreadOptaror.js
+++ src/UpdateObjWithSpreadOptaror.js
@@ -1,8 +1,19 @@
 import './UpdateObjWithSpreadOperator.css'
-import { useState } from "react";
+// import { useState } from "react";
+import { useImmer } from "use-immer";
+//use-immer->function send->function ma as a perameter obj pass krvo 
+//use-immer ma direct data puch thshe 
+//use-immer like use-State
 
 export default function FormGallary() {
-  const [person, setPerson] = useState({
+  // const [person, setPerson] = useState({
+  //   name: "Namrata",
+  //   title: "xcvcxgv",
+  //   city: "Berlin",
+  //   Image: "https://e0.pxfuel.com/wallpapers/355/72/desktop-wallpaper-cartoon-boy-iphone-cute-cartoon-boys-thumbnail.jpg",
+  // });
+
+  const [person, setPerson] = useImmer({
     name: "Namrata",
     title: "xcvcxgv",
     city: "Berlin",
@@ -10,29 +21,41 @@ export default function FormGallary() {
   });
 
   function handleNameChange(e) {
-    console.log(e);
-    setPerson({
-      ...person,
-      name: e.target.value,
+    // console.log(e);
+    // setPerson({
+    //   ...person,
+    //   name: e.target.value,
+    // });
+    setPerson((obj) =>{
+      obj.name = e.target.value;
     });
   }
   function handleTitleChange(e) {
-    console.log(e);
-    setPerson({
-      ...person,
-      title: e.target.value,
+    // console.log(e);
+    // setPerson({
+    //   ...person,
+    //   title: e.target.value,
+    // });
+    setPerson((obj)=>{
+      obj.title = e.target.value;
     });
   }
   function handleCityChange(e) {
-    setPerson({
-      ...person,
-      city: e.target.value,
+    // setPerson({
+    //   ...person,
+    //   city: e.target.value,
+    // });
+    setPerson((obj)=>{
+      obj.city = e.target.value;
     });
   }
   function handleImageChange(e) {
-    setPerson({
-      ...person,
-      Image: e.target.value,
+    // setPerson({
+    //   ...person,
+    //   Image: e.target.value,
+    // });
+    setPerson((obj)=>{
+      obj.Image = e.target.value;
     });
   }
   return (
